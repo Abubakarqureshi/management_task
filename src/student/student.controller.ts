d66ab1bfa7be4b1fb51e087b16aacfa3c16c4fd9
Asad <asad@maqware.com>
2023-02-25 15:00:31 -0600

    First commit in management task

---
+++ src/student/student.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get } from '@nestjs/common';
+import { StudentService } from './student.service';
+
+@Controller('student')
+export class StudentController {
+    constructor(private readonly managerService: StudentService) {}
+
+    @Get()
+    async getStatus(
+     
+    ) {
+      try {
+        const data = await this.managerService.findAll();
+        console.log(data)
+       return data
+      } catch (err) {
+      
+        };
+      }
+    }
+
+
